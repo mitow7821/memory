2e66ac7578b69bf16c8bf846b9ded63e42ece652
Dawid Stefanko <dawid.stefanko@grupa.itdesk.eu>
2022-07-15 08:55:53 +0200

    fix

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import className from "classnames";
 
 interface Props {
@@ -22,18 +22,6 @@ export default function Board({ board }: Props) {
     setSecondTile(null);
   }
 
-  function compareTiles() {
-    if (firstTile && secondTile && firstTile.value === secondTile.value) {
-      setPoints((e) => e + 1);
-
-      setFoundTiles((f) => [...f, firstTile.value]);
-    }
-
-    setTimeout(() => {
-      resetSelectedTiles();
-    }, 1000);
-  }
-
   function onTileClick(tile: SelectedTile) {
     console.log(tile);
     if (!firstTile) {
@@ -43,9 +31,23 @@ export default function Board({ board }: Props) {
     }
 
     setSecondTile(tile);
-    console.log(secondTile);
-    compareTiles();
   }
+
+  useEffect(() => {
+    if (!secondTile || !firstTile) {
+      return;
+    }
+
+    if (firstTile.value === secondTile.value) {
+      setPoints((e) => e + 1);
+
+      setFoundTiles((f) => [...f, firstTile.value]);
+    }
+
+    setTimeout(() => {
+      resetSelectedTiles();
+    }, 1000);
+  }, [secondTile, firstTile]);
 
   const isTileSelected = (tile: SelectedTile) =>
     [firstTile, secondTile].some(
@@ -72,13 +74,11 @@ export default function Board({ board }: Props) {
                 <div
                   key={xIndex}
                   onClick={() => onTileClick(tile)}
-                  className={
-                    "bg-gray-100 w-10 h-10" +
-                    className({
-                      "bg-red-500": isTileSelected(tile),
-                      "bg-green-500": isTileFound(tile.value),
-                    })
-                  }
+                  className={`bg-gray-100 w-10 h-10 ${className({
+                    "bg-red-500":
+                      isTileSelected(tile) && !isTileFound(tile.value),
+                    "!bg-green-500": isTileFound(tile.value),
+                  })}`}
                 >
                   {(isTileFound(tile.value) || isTileSelected(tile)) && value}
                 </div>
